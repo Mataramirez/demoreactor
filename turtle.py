db14428f303d9ca454f5dd00db3f1a044c1d9f4e
Mata <mataramirez@gmail.com>
2023-03-24 15:41:22 -0600

    new

--- turtle.py
+++ turtle.py
@@ -1,13 +1,13 @@
 import turtle
 import colorsys
-t = turtle.Turtle()
-s = turtle.Screen()
+t - turtle.Turtle()
+s - turtle.Screen()
 s.bgcolor("black")
 t.speed(0)
 n = 36
 h = 0
 for i in range(460):
-    c = colorsys.hsv_to_rgb(h.1.0.9)
+    c = colorsys.hsv_to_rgb(h,1,0.9)
     h += 1/n
     t.color(c)
     t.left(145)
